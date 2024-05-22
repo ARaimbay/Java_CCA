532799c4a8c96e9d4d9ad13202fa9ebdca206288
Aigerim Raimbay <raimbay89@gmail.com>
2024-05-22 10:34:07 -0500

    review of data types

---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+	public static void main(String[] args) {  
+    String name = "Aigerim";
+    int age = 34;
+    double desiredSalary = 150000.00;
+    char gender = 'f';
+    boolean lookingForJob = true;
+	}
+}
