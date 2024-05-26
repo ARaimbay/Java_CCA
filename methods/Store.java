3d71878600babb608c547332ca5508c664ace6ac
Aigerim Raimbay <raimbay89@gmail.com>
2024-05-26 15:08:13 -0500

    method w/o output

---
+++ methods/Store.java
@@ -0,0 +1,14 @@
+public class Store {
+    // instance fields
+    String productType;
+      
+    // constructor method
+    public Store(String product) {
+      productType = product;
+    }
+      
+    // Add advertise method below
+    public void advertise() {
+  
+    }
+  }
