3e664ca80f4cd7f86a557e8643dacb389337e556
Aigerim Raimbay <raimbay89@gmail.com>
2024-05-26 16:41:05 -0500

    adding parameters

---
+++ methods/Main1.java
@@ -0,0 +1,7 @@
+public class Main1{
+    public static void main(String[] args) {
+      Store lemonadeStand = new Store("Lemonade");
+      lemonadeStand.greetCustomer("Toyota");
+      
+    }
+  }
