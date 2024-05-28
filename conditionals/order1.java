77a2c86c9523edd320a48e274cf32b37b57258ec
Aigerim Raimbay <raimbay89@gmail.com>
2024-05-28 17:20:44 -0500

    if-then-else

---
+++ conditionals/order1.java
@@ -0,0 +1,17 @@
+public class Order1 {
+  
+    public static void main(String[] args) {
+      
+      boolean isFilled = false;
+      
+      // Write an if-then-else statement:
+      if(isFilled) {
+        System.out.println("Order not ready");
+      }
+      else {
+        System.out.println("Shipping");
+      }
+        
+    }
+    
+  }
