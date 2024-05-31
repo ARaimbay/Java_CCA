6dcedf2b10f0eeebc63d0c7e58101a7df8d01c80
Aigerim Raimbay <raimbay89@gmail.com>
2024-05-31 16:54:36 -0500

    get element with index

---
+++ Arrays_ArrayLists/Newsfeed2.java
@@ -0,0 +1,23 @@
+public class Newsfeed2 {
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    public int[] views = {0, 0, 0, 0};
+    
+    public Newsfeed(){
+  
+    }
+      
+    public String[] getTopics(){
+      return topics;
+    }
+    
+    public String getFirstTopic() {
+  
+      return topics [0];
+    }
+  
+    
+    public void viewTopic(int topicIndex){
+     
+      
+    }
+  }
