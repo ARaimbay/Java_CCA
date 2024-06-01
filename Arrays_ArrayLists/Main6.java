ed0693e9f4ce84691c33bb9223bdb88b7c86e043
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-01 15:15:31 -0500

    array with values, empty array, length

---
+++ Arrays_ArrayLists/Main6.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public static void main(String[] args){
+  String[] students = {"Sade", "Alexus", "Sam", "Koma"};
+  int[] mathScores = new int[4];
+  mathScores[0] = 64;
+  mathScores[2] = 76;
+  mathScores[1] = 57;
+  mathScores[3] = 98;
+  System.out.println("The number of the student in the class is " + students.length + ".");
+
+    
+}
