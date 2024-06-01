f04f28fee6a8021a928f2120da3a7c4d344537f0
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-01 14:35:51 -0500

    empty arrays

---
+++ Arrays_ArrayLists/Newsfeed3.java
@@ -0,0 +1,16 @@
+public class Newsfeed3 {
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    int[] views = {0, 0, 0, 0};
+    String[] favoriteArticles;
+    
+    public Newsfeed(){
+      // Initialize favoriteArticles here:
+      String[] favoriteArticles = new String[10];
+      
+    }
+    
+    public void setFavoriteArticle(int favoriteIndex, String newArticle){
+      // Add newArticle to favoriteArticles:
+      
+    }
+  }
