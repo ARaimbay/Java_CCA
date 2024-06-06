f4c064319eeec603a5864c67101c4bcd5553fb3e
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-06 12:21:30 -0500

    concat method

---
+++ string_method/CombineNames.java
@@ -0,0 +1,15 @@
+public class CombineNames {
+  
+	public static void main(String[] args) {
+    
+    // Add your first names:
+    String firstName = "Aigerim";  
+    String lastName = " Raimbay";
+
+    // What's the full name?
+    System.out.println(firstName.concat(" " + lastName));
+    
+    
+  }
+  
+}
