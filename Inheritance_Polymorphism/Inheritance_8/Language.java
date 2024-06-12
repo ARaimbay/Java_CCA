638cf8431148f47deb27e3a574f7a9f12b9cba43
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-12 14:24:10 -0500

    call subclass with change in constructor of subclass

--- Inheritance_Polymorphism/Inheritance_8/Language.java
+++ Inheritance_Polymorphism/Inheritance_8/Language.java
@@ -13,6 +13,18 @@ class Language{
     public static void main(String[] args){
       Language spanish = new Language("Spanish", 5550000, "Spain, Latin America and Guinea", "subject-verb-objet");
       spanish.getInfo();
+
+      Language kazakh = new Language("Kazakh", 19000000, "Kazakhstan", "subject-object-verb");
+      kazakh.getInfo();
+
+      Mayan kiche = new Mayan("Ki'che'", 2330000);
+      kiche.getInfo();
+
+      SinoTibetan mandarin = new SinoTibetan("Mandarin Chinese", 1110000);
+      mandarin.getInfo();
+      SinoTibetan burmese = new SinoTibetan("Burmese", 430000);
+      burmese.getInfo();
+
   
     }
     public void getInfo(){
