e016c0d4cec4ceca715e423ef9df74bea45e0044
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-12 13:32:02 -0500

    child classes in method parameters

---
+++ Inheritance_Polymorphism/Inheritance_7/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[] args) {
+      Noodle ramen, pho;
+      ramen = new Ramen();
+      pho = new Pho();
+        
+      NoodleRestaurant customer1 = new NoodleRestaurant("Sagirah");
+      customer1.order(pho);
+    
+      // Add your code here
+    
+    }
+  }
