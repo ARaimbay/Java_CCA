2cdb00b3d7fd8d9694f34b2ed81e57184c64f5d6
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-14 14:11:49 -0500

    reinitialize 2D array values with new values

--- 2D_arrays/declare_initialize_assign/Main.java
+++ 2D_arrays/declare_initialize_assign/Main.java
@@ -4,5 +4,6 @@ public class Main {
     float[][] floatTwoD;
     floatTwoD = new float[4][10];
     char[][] ticTacToe = {{'X','O','O'},{'O', 'X', ' '},{'X', ' ', 'X'}};
+    ticTacToe = new char[][] {{'O','X','X'},{'X', 'O', ' '},{'O', ' ', 'O'}};
     }
 }
