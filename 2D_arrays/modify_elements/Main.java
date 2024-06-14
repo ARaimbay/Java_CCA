ae012694b9c48832926f5f92056270aa400222f3
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-14 14:30:29 -0500

    new 2Darray

---
+++ 2D_arrays/modify_elements/Main.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class Main {
+	public static void main(String[] args) {
+		// Using the provided 2D array
+	  int[][] intMatrix = {
+				{1, 1, 1, 1, 1},
+				{2, 4, 6, 8, 0},
+				{9, 8, 7, 6, 5}
+		};   
+    
+		// Declare and initialize a 2D array called subMatrix
+		int[][] subMatrix = new int[2][2];
+    
+   //System.out.println(Arrays.deepToString(subMatrix));
+    }
+}
