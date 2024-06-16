a749e6d08a36ee5229f6690f260e6eadae6959ba
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-16 14:52:28 -0500

    new array matrix

---
+++ 2D_arrays/traversing_2DArrays/Review.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+public class Review {
+	public static void main(String[] args) {  
+
+		//Declare and initialize a 4x3 2D array of doubles called `scores` below
+    double[][] scores = {{80.4, -1, -1}, {96.2, -1, -1},{100.0, -1, -1},{78.9, -1, -1}};
+
+
+  	System.out.println(Arrays.deepToString(scores));
+
+		//Manually enter the scores for the second exam below
+
+
+  	System.out.println(Arrays.deepToString(scores));
+		
+  	//Declare and initialize an empty 4x2 2D array of double values called `newScores` below
+
+
+  	//Use `for` loops to copy the scores below
+    
+
+  	System.out.println(Arrays.deepToString(newScores));
+
+  	//Iterate through the `newScores` 2D array and use `if` statement to add 2 additional points below
+    
+    System.out.println(Arrays.deepToString(newScores));
+	}
+}
