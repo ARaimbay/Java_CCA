f2fd02d625b76f9212bda43d18e77b615d71c79a
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-16 14:19:57 -0500

    new array list

---
+++ 2D_arrays/traversing_2DArrays/Combi.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+public class Combi {
+	public static void main(String[] args) {
+		int[][] imageData={
+      {100,90,255,80,70,255,60,50},
+			{255,10,5,255,10,5,255,255},
+			{255,255,255,0,255,255,255,75},
+			{255,60,30,0,30,60,255,255}
+    };
+		// Declare and initialize the 2D array newImage
+    int[][] newImage = new int[4][6];
+		
+
+		// Add a nested `for` loop and copy the data of `imagedata` to `newImage`
+    
+
+		System.out.println(Arrays.deepToString(newImage));
+			
+		for(int i=0; i<newImage.length; i++){
+      for(int j=0; j<newImage[i].length; j++){
+        // Add the if-else statement here
+        
+      }
+    }
+		System.out.println(Arrays.deepToString(newImage));
+	}
+}
