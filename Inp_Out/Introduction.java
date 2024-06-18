e878aea5ae34077068eb35ba6483fa90d4b17c02
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-18 13:42:36 -0500

    print

--- Inp_Out/Introduction.java
+++ Inp_Out/Introduction.java
@@ -5,6 +5,7 @@ public class Introduction {
         int age = 34;
         System.out.println("My name is " + name);
         System.out.printf("I am %d years old\n", age);
+        System.out.print("My hobbies are " + hobbies);
   
     }
   }
