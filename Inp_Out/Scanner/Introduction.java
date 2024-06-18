edf41e94e46c6fc1f757f4a1e64519fcdedb3390
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-18 14:09:41 -0500

    input

--- Inp_Out/Scanner/Introduction.java
+++ Inp_Out/Scanner/Introduction.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Introduction {
   public static void main( String[] args ) {
     Scanner input = new Scanner(System.in);
+    System.out.println("What is your name?");
     
   }
 }
