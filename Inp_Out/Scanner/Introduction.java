5c6d9c437f43b93765a49ce50220d01af933c0f3
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-18 14:10:59 -0500

    next input

--- Inp_Out/Scanner/Introduction.java
+++ Inp_Out/Scanner/Introduction.java
@@ -3,6 +3,6 @@ public class Introduction {
   public static void main( String[] args ) {
     Scanner input = new Scanner(System.in);
     System.out.println("What is your name?");
-    
+    String userName = input.next();
   }
 }
