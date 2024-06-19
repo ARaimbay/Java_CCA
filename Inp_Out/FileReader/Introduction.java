84eb9cf14da8dc8a70932df7559f1c4747e210c6
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-19 14:32:46 -0500

    accessing file

--- Inp_Out/FileReader/Introduction.java
+++ Inp_Out/FileReader/Introduction.java
@@ -4,5 +4,10 @@ public class Introduction {
   public static void main(String[] args) throws IOException {
     String path = "./input.txt";
     FileReader reader = new FileReader(path);
+    int counter = 0;
+    while ((counter = reader.read()) != -1) {
+      System.out.print((char)counter);
   }
+  reader.close();
+}
 }
