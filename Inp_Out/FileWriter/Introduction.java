6d5fdbafeb0e8897e3abef1c23c917815d07b5f9
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-19 14:57:48 -0500

    output

--- Inp_Out/FileWriter/Introduction.java
+++ Inp_Out/FileWriter/Introduction.java
@@ -4,5 +4,8 @@ public class Introduction {
   public static void main(String[] args) throws IOException {
     // Your code here:
     FileWriter output = new FileWriter("output.txt");
+    String outputText = "Hello to me";
+    output.write(outputText);
   }
+  output.close();
 }
