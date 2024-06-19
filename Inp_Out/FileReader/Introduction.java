49b64c446012f8a8195345928c699f8277f8d8b0
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-19 14:20:38 -0500

    filereader

---
+++ Inp_Out/FileReader/Introduction.java
@@ -0,0 +1,7 @@
+import java.io.FileReader;
+import java.io.IOException;
+public class Introduction {
+  public static void main(String[] args) throws IOException {
+    String path = "./input.txt";
+  }
+}
