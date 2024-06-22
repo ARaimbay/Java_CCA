52356e4b044a9d76d8665daeba7dc212c2fb2fd3
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-22 16:00:42 -0500

    new objects added

--- Serialization/Serial_Obj_to_File/Car.java
+++ Serialization/Serial_Obj_to_File/Car.java
@@ -10,5 +10,7 @@ public class Car implements Serializable {
   private static final long serialVersionUID = 1L;
 
   public static void main(String[] args) throws FileNotFoundException, IOException {
+    Car toyota = new Car("Toyota", 2021);
+    Car honda = new Car("Honda", 2020);
   }
 }
