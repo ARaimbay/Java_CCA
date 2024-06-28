75262c75b52127457c3baf78f2d731e3cc142582
Aigerim Raimbay <raimbay89@gmail.com>
2024-06-28 16:59:20 -0500

    classes practice

---
+++ Generics_Collections/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      String myWord = "Hello";
+      Book myBook = new Book("My Book");
+      // Create `Container` references and print statement below...
+    }
+  }
