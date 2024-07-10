7fd6d5f015c891fa88ec0b9e4b2661512d39384c
Aigerim Raimbay <raimbay89@gmail.com>
2024-07-10 15:24:41 -0500

    constructots added

--- Generics_Collections/Container.java
+++ Generics_Collections/Container.java
@@ -1,4 +1,14 @@
 public class Container <T> {
     private T data;
+
+    public Container(T data) {
+        this.data = data;
+      }
+      public void setData(T data) {
+        this.data = data;
+      }
+      public T getData() {
+        return this.data;
+      }
   
   }
