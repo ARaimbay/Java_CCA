1bb7463b108ce4833a264de0d0e31f5c02cdd3d8
Aigerim Raimbay <raimbay89@gmail.com>
2024-07-10 15:43:38 -0500

    creating containers to call data within it

--- Generics_Collections/Main.java
+++ Generics_Collections/Main.java
@@ -1,9 +1,13 @@
+import java.awt.print.Book;
+
 public class Main {
     public static void main(String[] args) {
       String myWord = "Hello";
       Book myBook = new Book("My Book");
       // Create `Container` references and print statement below...
-      System.out.println(myWord);
-      System.out.println("Hello World");
+      Container<String> wordContainer = new Container<>(myWord);
+      Container<Book> bookContainer = new Container<>(myBook);
+      System.out.println(wordContainer.getData());
+      System.out.println(bookContainer.getData());
     }
   }
