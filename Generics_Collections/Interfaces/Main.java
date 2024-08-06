cd3b2cc2a1979915e100129c3d6435237c0079de
Aigerim Raimbay <raimbay89@gmail.com>
2024-08-06 12:21:48 -0500

    main

---
+++ Generics_Collections/Interfaces/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      int myNumber = 24;
+      String bookName = "Hello Book!";
+      // Enter your code below...
+  
+    }
+  }
