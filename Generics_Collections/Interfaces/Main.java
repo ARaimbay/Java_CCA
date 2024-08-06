a0cc46857d961a783a24144c7fa26ab4904ef565
Aigerim Raimbay <raimbay89@gmail.com>
2024-08-06 12:35:08 -0500

    output from generic interfaces

--- Generics_Collections/Interfaces/Main.java
+++ Generics_Collections/Interfaces/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
       int myNumber = 24;
       String bookName = "Hello Book!";
-      // Enter your code below...
+      
+      Retrieve<Integer> containerRetriever = new Container<>(myNumber);
+      Retrieve<String> bookRetriever = new Book(bookName);
+
+      System.out.println(containerRetriever.retrieveData());
+      System.out.println(bookRetriever.retrieveData());
   
     }
   }
